0c021d0cf70a3ae5a9371fe67279eb1d61cd6a7c
Tingxuan <stx17@163.com>
2018-11-27 01:06:56 +0800

    cal

--- calculate_centrality.py
+++ calculate_centrality.py
@@ -7,10 +7,12 @@ import time
 class CalculateCentrality:
     def __init__(self):
         # init graph
-        self.spammer_graph_path = 'graph/separate_spammer_771487.pickle'
-        self.spammer_graph = nx.read_gpickle(self.spammer_graph_path)
-        self.non_spammer_graph_path = 'graph/separate_non_spammer_771487.pickle'
-        self.non_spammer_graph = nx.read_gpickle(self.non_spammer_graph_path)
+        # self.spammer_graph_path = 'graph/separate_spammer_771487.pickle'
+        # self.spammer_graph = nx.read_gpickle(self.spammer_graph_path)
+        # self.non_spammer_graph_path = 'graph/separate_non_spammer_771487.pickle'
+        # self.non_spammer_graph = nx.read_gpickle(self.non_spammer_graph_path)
+        self.graph_path = 'graph/firendship_new_label209579.pickle'
+        self.graph = nx.read_gpickle(self.graph_path)
 
         # init mysql
         # self.db = MySQLdb.connect("localhost", "root", "stx11stx11", "yelp_data", charset="utf8")
@@ -20,18 +22,19 @@ class CalculateCentrality:
     #     self.cursor.commit()
 
     def summary(self):
-        print self.spammer_graph.number_of_nodes()
-        print self.spammer_graph.number_of_edges()
-        print self.non_spammer_graph.number_of_nodes()
-        print self.non_spammer_graph.number_of_edges()
+        # print self.spammer_graph.number_of_nodes()
+        # print self.spammer_graph.number_of_edges()
+        # print self.non_spammer_graph.number_of_nodes()
+        # print self.non_spammer_graph.number_of_edges()
+        print self.graph.number_of_nodes()
 
     def degree_centrality(self):
 
         insert_item_list = list()
 
         # spammer centrality
-        for num, node in enumerate(self.spammer_graph.nodes()):
-            item = (node, str(self.spammer_graph.degree(node)))
+        for num, node in enumerate(self.graph.nodes()):
+            item = (node, str(self.graph.degree(node)))
             insert_item_list.append(item)
             if num % 1000 == 0:
                 print num
@@ -48,8 +51,17 @@ class CalculateCentrality:
         #     if num == 2:
         #         break
         #     print self.spammer_graph.betweeness(node)
+
+        # get the major part of the graph
+        connected_subgraphs = list(nx.connected_components(self.graph))
+        print len(connected_subgraphs)
+        for i, sub in enumerate(connected_subgraphs):
+            print i
+            print 'number', len(sub)
+
+        # connected_graph = connected_subgraphs[0]
         print "begin betweenness cal", time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-        all_node_betweenness = nx.betweenness_centrality(self.spammer_graph)
+        all_node_betweenness = nx.betweenness_centrality(self.graph)
         print "end betweenness cal", time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
 
         for iter, item in enumerate(all_node_betweenness):
@@ -77,4 +89,4 @@ if __name__ == '__main__':
     CC.summary()
 
     # CC.degree_centrality()
-    CC.betweeness_centrality()
+    CC.betweenness_centrality()
