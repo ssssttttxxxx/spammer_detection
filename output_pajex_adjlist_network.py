585fdb904341e08f169f81fb475b4f8f7f083760
stx <stx17@163.com>
2018-12-09 13:07:36 +0800

    rename

--- output_pajex_adjlist_network.py
+++ output_pajex_adjlist_network.py
@@ -2,9 +2,9 @@
 
 import networkx as nx
 
-graph_path = 'graph/high_degree_partition_2.pickle'
+graph_path = 'graph/old_friendship_connected.pickle'
 graph = nx.read_gpickle(graph_path)
 
 # nx.write_pajek(graph, 'pajek/separate_spammer_pajek.net')
 # nx.write_pajek(graph, 'pajek/separate_spammer_771487.net')
-nx.write_adjlist(graph, 'graph/high_degree_partition_2.adjlist')
+nx.write_adjlist(graph, 'graph/old_friendship_connected.adjlist')
